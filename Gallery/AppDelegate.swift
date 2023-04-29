010aa77bc70eb6827ec38488214c5079fe5747e8
sonya <sofiya.fedorova@mail.ru>
2023-04-30 00:40:04 +0300

    init project

---
+++ Gallery/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Gallery
+//
+//  Created by Sonya Fedorova on 29.04.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
---
+++ Gallery/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Gallery
+//
+//  Created by Sonya Fedorova on 29.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
